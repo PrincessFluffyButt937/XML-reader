0513086e1be2b5301c5ae19cbd73df52cc5ba24e
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-09-27 20:59:51 +0200

    basic tracibility exctraction done

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import xml.etree.ElementTree as ET
 # "panel" = hides references + their mounted IDs
 # "charge" = actuall component data pairable to IDs
 
+#"2025091504150200" - time format
+#"2025 - year[0-3], 09 -month[4-5], 15 - day[6-7], 04 - hours[8-9], 15 - min[10-11] 02 - seconds[12-13] 00-??"
 
 def ref_des_finder(ref_list, xml_path):
     #returns reference matches from a single xml file in dictionary format -> "ID": set{ref1, ref2}
@@ -26,6 +28,30 @@ def ref_des_finder(ref_list, xml_path):
     print(matches)
     return matches
 
+def get_component_data_from_id(id_dict, xml_path):
+    xml_tree = ET.parse(xml_path)
+    xml_root = xml_tree.getroot()
+    # use xml_root.attrib to capture time stamps + serial number
+    print(xml_root.attrib)
+    component_data = {}
+    for branch in xml_root:
+        if branch.tag == "charge":
+            for id in id_dict:
+                if id == branch.attrib["id"]:
+                    temp_time = xml_root.attrib["dateComplete"]
+                    if len(temp_time) != 16:
+                        formated_time = "UnknownTimeFormat"
+                    else:
+                        formated_time = f"{temp_time[6:8]}/{temp_time[4:6]}/{temp_time[0:4]} {temp_time[8:10]}:{temp_time[10:12]}:{temp_time[12:14]}"
+                    print(formated_time)
+                    #set which contains references is converted into string -> set/list.. cannot be used as keys in dictionary.. maybe earlier conversion?
+                    component_data[f"{id_dict[id]}"] = {"PN": branch.attrib["barc1"], "HU": branch.attrib["barc6"], "LC": branch.attrib["barc2"], "TS": formated_time}
+    print(component_data)
+    #returns nested dictionary
+    #example with real keys -> {"{'Q1'}": {'PN': '20005985', 'HU': '001014656171', 'LC': '012218', 'TS': '25/04/2024 04:09:11'}}
+    return component_data
+
+
 
 def main():
     M_path = list(os.path.split(os.path.abspath(__file__)))
@@ -42,6 +68,9 @@ def main():
     
     test_list = ["C8", "C4", "Q1"]
 
-    ref_des_finder(test_list, xml_filepath)
+    res_found = ref_des_finder(test_list, xml_filepath)
+    tracibility_data = get_component_data_from_id(res_found, xml_filepath)
+
 main()
+
 

d7dde74975a47b324af6a94fcb3da02f25bfaf37
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-10-16 20:23:25 +0200

    script mode decoder added

--- functions.py
+++ functions.py
@@ -1,9 +1,125 @@
 import os
 
+from enum import Enum
+
 import xml.etree.ElementTree as ET
 import xlsxwriter
 
 from data import Data, Trace, ref_to_str
+
+class Mode(Enum):
+    #search mode__input mode__output format
+    SN_TEXT_TXT = 1
+    SN_TEXT_XLS = 2
+    SN_PATH_TXT = 3
+    SN_PATH_XLS = 4
+
+    HU_TEXT_TXT = 5
+    HUC_TEXT_TXT = 6
+    HU_PATH_TXT = 7
+    HUC_PATH_TXT = 8
+
+    HU_TEXT_XLS = 9
+    HUC_TEXT_XLS = 10
+    HU_PATH_XLS = 11
+    HUC_PATH_XLS = 12
+
+def get_script_mode(command_str):
+    if len(command_str) != 4 or not command_str.startswith("-"):
+        return f"Invalid mode format: {command_str}"
+    s, h, c = False, False, False
+    t, x = False, False
+    r, p = False,
+    for i in range(1, 4):
+        if command_str[i] == "s":
+            if h or c:
+                return "Invalid mode \"s\" is not allowed to be selected together with either \"h\" or \"c\"."
+            elif s:
+                return "Invalid mode - \"s\" selected multiple times"
+            else:
+                s = True
+        elif command_str[i] == "h":
+            if s or c:
+                return "Invalid mode \"h\" is not allowed to be selected together with either \"s\" or \"c\"."
+            elif h:
+                return "Invalid mode - \"h\" selected multiple times"
+            else:
+                h = True
+        elif command_str[i] == "c":
+            if s or h:
+                return "Invalid mode \"c\" is not allowed to be selected together with either \"s\" or \"h\"."
+            elif c:
+                return "Invalid mode - \"c\" selected multiple times"
+            else:
+                c = True
+        elif command_str[i] == "t":
+            if x:
+                return "Invalid mode \"t\" is not allowed to be selected together with \"x\"."
+            elif t:
+                return "Invalid mode - \"t\" selected multiple times"
+            else:
+                t = True  
+        elif command_str[i] == "x":
+            if t:
+                return "Invalid mode \"x\" is not allowed to be selected together with \"t\"."
+            elif x:
+                return "Invalid mode - \"x\" selected multiple times"
+            else:
+                x = True  
+        elif command_str[i] == "r":
+            if p:
+                return "Invalid mode \"r\" is not allowed to be selected together with \"p\"."
+            elif r:
+                return "Invalid mode - \"r\" selected multiple times"
+            else:
+                r = True  
+        elif command_str[i] == "p":
+            if r:
+                return "Invalid mode \"p\" is not allowed to be selected together with \"r\"."
+            elif p:
+                return "Invalid mode - \"p\" selected multiple times"
+            else:
+                p = True  
+        else:
+            return f"Invalid mode: \"{command_str[i]}\" is not recognized by this script."
+    #mode selection below
+    #serial number search variants
+    if s:
+        if r:
+            if t:
+                return Mode.SN_TEXT_TXT
+            if x:
+                return Mode.SN_TEXT_XLS   
+        if p:
+            if t:
+                return Mode.SN_PATH_TXT
+            if x:
+                return Mode.SN_PATH_XLS
+    #handling unit search - partial variants
+    if h:
+        if r:
+            if t:
+                return Mode.HU_TEXT_TXT
+            if x:
+                return Mode.HU_TEXT_XLS  
+        if p:
+            if t:
+                return Mode.HU_PATH_TXT
+            if x:
+                return Mode.HU_TEXT_XLS
+    #handling unit search - complete variants
+    if c:
+        if r:
+            if t:
+                return Mode.HUC_TEXT_TXT
+            if x:
+                return Mode.HUC_TEXT_XLS  
+        if p:
+            if t:
+                return Mode.HUC_PATH_TXT
+            if x:
+                return Mode.HUC_PATH_XLS
+        
 
 def dest_check(dest_path):
     if os.path.exists(dest_path):
--- main.py
+++ main.py
@@ -7,13 +7,32 @@ import sys
 #"2025091504150200" - time format
 #"2025 - year[0-3], 09 -month[4-5], 15 - day[6-7], 04 - hours[8-9], 15 - min[10-11] 02 - seconds[12-13] 00-??"
 
-modes = ["SN", "HU"]
-out_formats = [".txt", ".xls"]
+'''
+Flags:
+search:
+-s = serial number search
+-h = handling unit search (partial sn search)
+-c = full tracibility of located handling units (takes twice as long)
 
+Output formats:
+t = text
+x = excel table
+Input format
+r = raw string
+p = filepath
+'''
 
             
 def main():
     command = sys.argv
+    '''
+    command[0] = main.py
+    command[1] = flags
+    command[2] = input - serial number / handling unit / filepath to read multiple serial numbers from.
+    '''
+    flags = command[1]
+    input_string = command[2]
+
     print(command)
 
     #with open(rep_path, "w") as file:
